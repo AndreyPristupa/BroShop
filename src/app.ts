93a49850e01072deb04fff28f4bc9a6c6bb030bc
Andrii Prystupa <a.prystupa@kiev.astoundcommerce.com>
2016-08-26 16:45:59 +0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,118 @@
+/// <reference path="_all.d.ts" />
+"use strict";
+
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as path from "path";
+import * as session from "express-session"
+
+import * as indexRoute from "./routes/index";
+
+let MongoSessionStore = require("connect-mongo")(session);
+//todo all configs must be taken from ENV
+let config = require('./config.json')
+
+/**
+ * The server.
+ *
+ * @class Server
+ */
+class Server {
+
+  public app: express.Application;
+
+  /**
+   * Bootstrap the application.
+   *
+   * @class Server
+   * @method bootstrap
+   * @static
+   */
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  /**
+   * Constructor.
+   *
+   * @class Server
+   * @constructor
+   */
+  constructor() {
+    this.app = express();
+
+    //configure application
+    this.config();
+
+    //configure routes
+    this.routes();
+  }
+
+  /**
+   * Configure application
+   *
+   * @class Server
+   * @method config
+   * @return void
+   */
+  private config() {
+    //configure jade
+    this.app.set("views", path.join(__dirname, "views"));
+    this.app.set("view engine", "jade");
+
+    //mount logger
+    //this.app.use(logger("dev"));
+
+    //mounting mongodb and mongo session store
+
+    this.app.use(session({
+      store: new MongoSessionStore({ url: config.connection.url }),
+      cookie: { maxAge: new Date(Date.now() + 181440000) },
+      secret: config.authSecret
+    }))
+
+    //mount json form parser
+    this.app.use(bodyParser.json());
+
+    //mount query string parser
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+
+    //add static paths
+    this.app.use(express.static(path.join(__dirname, "public")));
+    this.app.use(express.static(path.join(__dirname, "bower_components")));
+
+    // catch 404 and forward to error handler
+    this.app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+      var error = new Error("Not Found");
+      err.status = 404;
+      next(err);
+    });
+  }
+
+  /**
+   * Configure routes
+   *
+   * @class Server
+   * @method routes
+   * @return void
+   */
+  private routes() {
+    //get router
+    let router: express.Router;
+    router = express.Router();
+
+    //create routes
+    var index: indexRoute.Index = new indexRoute.Index();
+    var profile: indexRoute.Profile = new indexRoute.Profile();
+
+    //home page
+    router.get("/", index.index.bind(index.index));
+    router.get("/profile", profile.index.bind(profile.index))
+
+    //use router middleware
+    this.app.use(router);
+  }
+}
+
+var server = Server.bootstrap();
+export = server.app;
